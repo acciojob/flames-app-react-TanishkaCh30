e6909c06d579f1b764baefb6d17c124d587ac52a
Tanishka Chaudhary <chaudharytanishka38@gmail.com>
2025-04-23 00:07:36 +0530

    commit2

--- src/components/Flames.js
+++ src/components/Flames.js
@@ -1,17 +1,120 @@
+// import React, { useState } from "react";
+
+// const FlamesApp = () => {
+
+//     let [val, setVal] = useState("");
+//     let [val2, setVal2] = useState("");
+//     let [val3, setVal3] = useState("");
+
+
+//     function calculateFuture() {
+
+//         if (val.trim() === "" || val2.trim() === "") {
+//             setVal3("Please Enter valid input");
+//             return
+//         }
+
+//         let s1 = val.toLowerCase();
+//         let s2 = val2.toLowerCase();
+
+//         let arr1 = s1.split("");
+//         let arr2 = s2.split("");
+//         let newArr = [];
+//         let arr2Copy =[...arr2];
+//         for (let i = 0; i < arr1.length; i++) {
+//             let found = false;
+
+//             for (let j = 0; j < arr2Copy.length; j++) {
+//                 if (arr1[i] === arr2Copy[j]) {
+//                     arr2Copy.splice(j, 1);
+//                     found = true;
+//                     break;
+//                 }
+//             }
+
+//             if (!found) {
+//                 newArr.push(arr1[i]);
+//             }
+//         }
+
+
+//         newArr = newArr.concat(arr2Copy);
+
+
+
+//         let length = newArr.length;
+
+//         let mod = length % 6;
+
+//         if (mod === 1) {
+//             setVal3("Friends");
+//         }
+//         else if (mod === 2) {
+//             setVal3("Love");
+//         }
+//         else if (mod === 3) {
+//             setVal3("Affection");
+//         }
+//         else if (mod === 4) {
+//             setVal3("Marriage");
+//         }
+//         else if (mod === 5) {
+//             setVal3("Enemy");
+//         }
+//         else if (mod === 0) {
+//             setVal3("Siblings");
+//         }
+
+//     }
+
+//     function clearData() {
+//         setVal("");
+//         setVal2("");
+//         setVal3("");
+//     }
+
+//     return (
+
+//         <div>
+//             <input type="text"
+//                 data-testid="input1"
+//                 name="name1"
+//                 onChange={((e) => {
+//                     setVal(e.target.value);
+//                 })}
+//                 value={val}></input>
+
+//             <input type="text"
+//                 data-testid="input2"
+//                 name="name2"
+//                 onChange={((e) => {
+//                     setVal2(e.target.value);
+//                 })}
+//                 value={val2}></input>
+
+
+//             <button data-testid="calculate_relationship" name="calculate_relationship" onClick={calculateFuture}>Calculate Relationship Future</button>
+//             <button data-testid="clear" name="clear" onClick={clearData}>Clear</button>
+//             <h3 data-testid="answer" value={val3}>{val3}</h3>
+//         </div>
+//     )
+// }
+
+
+// export default FlamesApp;
+
 import React, { useState } from "react";
 
 const FlamesApp = () => {
-
+    // Initialize with empty strings instead of undefined
     let [val, setVal] = useState("");
     let [val2, setVal2] = useState("");
     let [val3, setVal3] = useState("");
 
-
     function calculateFuture() {
-
         if (val.trim() === "" || val2.trim() === "") {
             setVal3("Please Enter valid input");
-            return
+            return;
         }
 
         let s1 = val.toLowerCase();
@@ -20,7 +123,10 @@ const FlamesApp = () => {
         let arr1 = s1.split("");
         let arr2 = s2.split("");
         let newArr = [];
-        let arr2Copy =[...arr2];
+
+        // Create a copy of arr2 to avoid modifying the original during iteration
+        let arr2Copy = [...arr2];
+
         for (let i = 0; i < arr1.length; i++) {
             let found = false;
 
@@ -37,68 +143,69 @@ const FlamesApp = () => {
             }
         }
 
-
+        // Add remaining characters from arr2Copy
         newArr = newArr.concat(arr2Copy);
 
-
-
         let length = newArr.length;
-
         let mod = length % 6;
 
         if (mod === 1) {
             setVal3("Friends");
-        }
-        else if (mod === 2) {
+        } else if (mod === 2) {
             setVal3("Love");
-        }
-        else if (mod === 3) {
+        } else if (mod === 3) {
             setVal3("Affection");
-        }
-        else if (mod === 4) {
+        } else if (mod === 4) {
             setVal3("Marriage");
-        }
-        else if (mod === 5) {
+        } else if (mod === 5) {
             setVal3("Enemy");
+        } else if (mod === 0) {
+            setVal3("Siblings"); // Fixed typo in "Siblings"
         }
-        else if (mod === 0) {
-            setVal3("Siblings");
-        }
-
     }
 
     function clearData() {
+        // Set to empty strings, not spaces
         setVal("");
         setVal2("");
         setVal3("");
     }
 
     return (
-
         <div>
-            <input type="text"
+            <input
+                type="text"
                 data-testid="input1"
                 name="name1"
-                onChange={((e) => {
+                onChange={(e) => {
                     setVal(e.target.value);
-                })}
-                value={val}></input>
+                }}
+                value={val}
+            ></input>
 
-            <input type="text"
+            <input
+                type="text"
                 data-testid="input2"
                 name="name2"
-                onChange={((e) => {
+                onChange={(e) => {
                     setVal2(e.target.value);
-                })}
-                value={val2}></input>
+                }}
+                value={val2}
+            ></input>
 
-
-            <button data-testid="calculate_relationship" name="calculate_relationship" onClick={calculateFuture}>Calculate Relationship Future</button>
-            <button data-testid="clear" name="clear" onClick={clearData}>Clear</button>
-            <h3 data-testid="answer" value={val3}>{val3}</h3>
+            <button
+                data-testid="calculate_relationship"
+                name="calculate_relationship"
+                onClick={calculateFuture}
+            >
+                Calculate Relationship Future
+            </button>
+            <button data-testid="clear" name="clear" onClick={clearData}>
+                Clear
+            </button>
+            <h3 data-testid="answer">{val3}</h3>
         </div>
-    )
-}
-
+    );
+};
 
 export default FlamesApp;
